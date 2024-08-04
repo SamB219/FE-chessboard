0c8abaca3bbfdd1b7737450e4ab5cfee2a6f7f73
SamB219 <morebagelsfr@gmail.com>
2024-08-03 22:59:41 -0400

    slight changes to the navBar WIP still

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+
+// Icon Imports
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { FaChessKing, FaHome } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { ImEye } from "react-icons/im";
+import { SlPuzzle } from "react-icons/sl";
+import { BiSolidDonateHeart } from "react-icons/bi";
 
 
 function NavBar() {
     return (
-        <Navbar className='NavBar'>
-            <Container>
-                <Navbar.Brand href="/home">Home</Navbar.Brand>
-                <Navbar.Brand href="/play">Play</Navbar.Brand>
-                <Navbar.Brand href="/puzzles">Puzzles</Navbar.Brand>
-                <Navbar.Brand href="/watch">Watch</Navbar.Brand>
-                <Navbar.Brand href="/profile">Profile</Navbar.Brand>
-                <Navbar.Brand href="/donate">Donate</Navbar.Brand>
+        <Navbar className="NavBar">
+            <Container className="nav-container">
+                <Navbar.Brand href="/home" className="nav-item">
+                    <FaHome /> Home
+                </Navbar.Brand>
+                <Nav className="nav-links">
+                    <Nav.Link href="/play" className="nav-item">
+                        <FaChessKing /> Play
+                    </Nav.Link>
+                    <Nav.Link href="/puzzles" className="nav-item">
+                        <SlPuzzle /> Puzzles
+                    </Nav.Link>
+                    <Nav.Link href="/watch" className="nav-item">
+                        <ImEye /> Watch
+                    </Nav.Link>
+                    <Nav.Link href="/profile" className="nav-item">
+                        <CgProfile /> Profile
+                    </Nav.Link>
+                    <Nav.Link href="/donate" className="nav-item">
+                        <BiSolidDonateHeart /> Donate
+                    </Nav.Link>
+                </Nav>
             </Container>
         </Navbar>
     );
